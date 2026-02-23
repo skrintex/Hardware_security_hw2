066f2815c34edab306c6b9afb3fbc488c4ca4551
Robert <rnb2135@columbia.edu>
2026-02-23 16:23:09 -0500

    changed to vcs now

--- AES-master/aes_check.sh
+++ AES-master/aes_check.sh
@@ -6,15 +6,19 @@ ct="$3"
 
 # Auto-detect AES size
 case ${#key} in
-  32) cipher="aes-128-ecb" ;;
-  48) cipher="aes-192-ecb" ;;
-  64) cipher="aes-256-ecb" ;;
-  *)  echo "Invalid key length"; exit 1 ;;
+32) cipher="aes-128-ecb" ;;
+48) cipher="aes-192-ecb" ;;
+64) cipher="aes-256-ecb" ;;
+*)
+  echo "Invalid key length"
+  exit 1
+  ;;
 esac
 
-out=$(echo "$pt" | xxd -r -p | \
-  openssl enc -"$cipher" -K "$key" -nosalt -nopad | \
+out=$(echo "$pt" | xxd -r -p |
+  openssl enc -"$cipher" -K "$key" -nosalt -nopad |
   xxd -p -c 16)
 
 echo "Expected: $ct"
 echo "Got:      $out"
+
